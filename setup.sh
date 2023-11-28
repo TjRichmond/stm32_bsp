fd2c7459588a6ed03d1b9bd2398ea5d2e098d21f
TjRichmond <tjrichmond99@gmail.com>
2023-11-27 23:53:45 -0800

    Added message to uncomment cmsis preprocessor for intellisense in vscode

--- setup.sh
+++ setup.sh
@@ -2,3 +2,5 @@ cd CMSIS/Device
 mkdir ST
 cd ST
 git clone https://github.com/STMicroelectronics/cmsis_device_u5 STM32U5
+# Add step to uncomment line 65 & remove '\' from line 64 in stm32u5xx.h 
+cd STM32U5/Include
