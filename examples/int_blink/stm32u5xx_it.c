ef026222d4cc18f0471cd3f2a04ca4941d131bc9
TjRichmond <tjrichmond99@gmail.com>
2024-02-13 18:52:28 -0800

    Refactored project folder structure to have startup code outside of examples

--- examples/int_blink/stm32u5xx_it.c
+++
@@ -1,8 +0,0 @@
-#include "stm32u5xx_it.h"
-#include "stm32u575xx.h"
-
-void tim6_handler(void)
-{
-  // Clear the interrupt flag to reset interrupt
-  TIM6->SR &= ~(TIM_SR_UIF);
-}
--- examples/int_blink/stm32u5xx_it.h
+++
@@ -1,6 +0,0 @@
-#ifndef _STM32U5xx_IT_H_
-#define _STM32U5xx_IT_H_
-
-void tim6_handler(void);
-
-#endif
