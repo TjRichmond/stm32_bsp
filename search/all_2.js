8d71a045ffa58291120682d9331936ac681b4d5e
TjRichmond <TjRichmond@users.noreply.github.com>
2024-02-14 02:56:50 +0000

    deploy: b03d22fac8d59b9c645a457e88c2ce1cff39623f

--- search/all_2.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['timerbasicclkreset_8',['TimerBasicClkReset',['../dc/d09/bsp__timer_8c.html#a61bd3c165154719a6d91b64b68e77b1d',1,'bsp_timer.c']]],
-  ['timerbasicinitmicrosec_9',['TimerBasicInitMicroSec',['../dc/d09/bsp__timer_8c.html#a0c110658ed69fef827f5167e50e42a3f',1,'bsp_timer.c']]],
-  ['timerbasicinitmillisec_10',['TimerBasicInitMilliSec',['../dc/d09/bsp__timer_8c.html#a838c23505cce2bd818ca5c7d2d8de173',1,'bsp_timer.c']]],
-  ['timerbasicinitsec_11',['TimerBasicInitSec',['../dc/d09/bsp__timer_8c.html#a40e95060289b57f712577af2d1834ebb',1,'bsp_timer.c']]]
-];
--- search/functions_0.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['gpioclearoutput_14',['GpioClearOutput',['../d3/d52/bsp__gpio_8c.html#abd5a4d5108b5ae01cf405a2a28c125ec',1,'bsp_gpio.c']]],
-  ['gpiopininit_15',['GpioPinInit',['../d3/d52/bsp__gpio_8c.html#a30e1e4de0065e122c6c12055a4285180',1,'bsp_gpio.c']]],
-  ['gpiopinmode_16',['GpioPinMode',['../d3/d52/bsp__gpio_8c.html#a7e7e138311ae6dd6e7af16a73e274553',1,'bsp_gpio.c']]],
-  ['gpiosetoutput_17',['GpioSetOutput',['../d3/d52/bsp__gpio_8c.html#a7c42728fcd853963bb8502be28a9a6cd',1,'bsp_gpio.c']]],
-  ['gpiosetpullreg_18',['GpioSetPullReg',['../d3/d52/bsp__gpio_8c.html#ad2c2a5b0a103b2b4a84b8b34239b05ae',1,'bsp_gpio.c']]],
-  ['gpiotoggleoutput_19',['GpioToggleOutput',['../d3/d52/bsp__gpio_8c.html#a1107487d80a856739b0c9677c0f22913',1,'bsp_gpio.c']]]
-];
