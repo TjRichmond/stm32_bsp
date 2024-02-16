b270d693558c9c21b05c545687d95e7fbeacef41
TjRichmond <TjRichmond99@gmail.com>
2024-02-16 02:57:34 -0800

    Updated doxygen comments in all bsp files

--- bsp/include/bsp_gpio.h
+++ bsp/include/bsp_gpio.h
@@ -1,6 +1,6 @@
 // MIT License
 
-// Copyright (c) 2023 Tristan Richmond
+// Copyright (c) 2024 Tristan Richmond
 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -20,6 +20,15 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+/**
+ * @file bsp_gpio.h
+ * @brief Header for using gpio pins
+ * 
+ * This header contains functions to control gpio pins hardware for
+ * the STM32U575. These functions include initializing, reading, and
+ * write values to the GPIO pins.
+*/
+
 #ifndef _BSP_GPIO_H_
 #define _BSP_GPIO_H_
 
@@ -27,37 +36,112 @@
 
 #define MAX_GPIO_PORT_PINS (16)
 
-// GPIO pin modes
+/**
+ * @brief Enumeration representing supported gpio pin modes
+ * 
+ * The STM32U575 supports up to 4 modes that determines how the
+ * pin behaves.
+ * - Input mode will receive digital signals
+ * - Ouput mode will transmit digital signals
+ * - Alternative function mode will behave as a different hardware controller
+ * - Analog mode will remain floating and not engage with core
+*/
 typedef enum pinMode{
+  /** @brief Input mode*/
   GPIO_INPUT_MODE = 3,
+  
+  /** @brief Output mode*/
   GPIO_OUTPUT_MODE = 2,
+
+  /** @brief Alternate function mode*/
   GPIO_ALT_FUNC_MODE = 1,
+
+  /** @brief Analog/floating mode*/
   GPIO_ANALOG_MODE = 0
 } PinMode;
 
-// GPIO pin pull resistor register modes
+/**
+ * @brief Enumeration representing supported gpio resistor pull directions
+ * 
+ * The STM32U575 supports up to 3 modes that determines how the
+ * pin behaves.
+ * - No pull mode will default the pin to floating 
+ * - Pull up mode will default the pin to logic high
+ * - Pull down mode will default the pin to logic low
+*/
 typedef enum pullMode {
+  /** @brief No pull mode*/
   GPIO_NO_PULL = 3,
+
+  /** @brief Pull up resistor mode*/
   GPIO_PULL_UP = 2,
+
+  /** @brief Pull down resistor mode*/
   GPIO_PULL_DOWN = 1
 } PullMode;
 
-// Init pin gpio
+/**
+ * @brief Initialize a gpio pin configuration
+ * 
+ * The gpio pin will be configured as an input/output and which pull mode
+ * @param port gpio port
+ * @param pin gpio pin
+ * @param pinMode input/output mode
+ * @param pullMode resistor pull up/down mode
+ * @return true/false value to indicate success of function 
+*/
 uint8_t GpioPinInit(GPIO_TypeDef *, uint8_t , PinMode , PullMode);
 
-// Set pin mode
+/**
+ * @brief Set the direction mode for a gpio pin 
+ * 
+ * The gpio pin will be configured as an input/output
+ * @param port gpio port
+ * @param pin gpio pin
+ * @param mode input/output mode
+ * @return true/false value to indicate success of function 
+*/
 uint8_t GpioPinMode(GPIO_TypeDef *, uint8_t, PinMode);
 
-// Set pin output value
+/**
+ * @brief Set the resistor pull direction
+ * 
+ * The gpio pin resistor will be configured to be pull up or pull down
+ * @param port gpio port
+ * @param pin gpio pin
+ * @param mode pull up/down mode
+ * @return true/false value to indicate success of function
+*/
 uint8_t GpioSetOutput(GPIO_TypeDef *, uint8_t);
 
-// Clear pin output value
+/**
+ * @brief Set the output value of the pin to high
+ * 
+ * The gpio pin output value will be set high or 1
+ * @param port gpio port
+ * @param pin gpio pin
+ * @return true/false value to indicate success of function
+*/
 uint8_t GpioClearOutput(GPIO_TypeDef *, uint8_t);
 
-// Toggle pin output
+/**
+ * @brief Set the output value of the pin to low
+ * 
+ * The gpio pin output value will be cleared to low or 0
+ * @param port gpio port
+ * @param pin gpio pin
+ * @return true/false value to indicate success of function
+*/
 uint8_t GpioToggleOutput(GPIO_TypeDef *, uint8_t);
 
-// Set pull up/down resistor mode
+/**
+ * @brief Toggle the output value of the pin
+ * 
+ * The gpio pin output value will be toggled
+ * @param port gpio port
+ * @param pin gpio pin
+ * @return true/false value to indicate success of function
+*/
 uint8_t GpioSetPullReg(GPIO_TypeDef *, uint8_t, PullMode);
 
 #endif
--- bsp/include/bsp_timer.h
+++ bsp/include/bsp_timer.h
@@ -1,6 +1,6 @@
 // MIT License
 
-// Copyright (c) 2023 Tristan Richmond
+// Copyright (c) 2024 Tristan Richmond
 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -20,17 +20,57 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+/**
+ * @file bsp_timer.h
+ * @brief Module for using basic hardware timer
+ * 
+ * This header contains functions to control 1 of 2 basic hardware timers
+ * for the STM32U575. These functions include initializing and reseting the 
+ * timers.
+*/
+
 #ifndef _BSP_TIMER_H_
 #define _BSP_TIMER_H_
 
 #include "stm32u575xx.h"
 
+/**
+ * @brief Initialize a basic microsecond timer
+ * 
+ * The basic timer will be configured to count in microseconds
+ * @param timer timer
+ * @param usec number of microseconds
+ * @return true/false value to indicate success of function
+*/
 uint8_t TimerBasicInitMicroSec(TIM_TypeDef *, uint16_t);
 
+/**
+ * @brief Initialize a basic millisecond timer
+ * 
+ * The basic timer will be configured to count in milliseconds
+ * @param timer timer
+ * @param msec number of milliseconds
+ * @return true/false value to indicate success of function
+*/
 uint8_t TimerBasicInitMilliSec(TIM_TypeDef *, uint16_t);
 
+/**
+ * @brief Initialize a basic second timer
+ * 
+ * The basic timer will be configured to count in seconds
+ * @param timer timer
+ * @param sec number of seconds
+ * @return true/false value to indicate success of function
+*/
 uint8_t TimerBasicInitSec(TIM_TypeDef *, uint16_t);
 
+/**
+ * @brief Reset a basic timer 
+ * 
+ * The basic timer will be reset its counter to 0 
+ * @param timer timer
+ * @return true/false value to indicate success of function
+*/
 uint8_t TimerBasicClkReset(TIM_TypeDef *);
 
 #endif
